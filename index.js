e0ad3b0f894fa333b70743adb62963215a5ba521
brunogcpinheiro <brunogcpinheiro@hotmail.com>
2018-04-23 14:28:18 -0300

    Create Users Table

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const nunjucks = require('nunjucks');
 
 const app = express();
 
+const { User } = require('./app/models');
+
+User.create({ name: 'Bruno', email: 'brunogcarpinellip@gmail.com', password: '123456' });
+
 nunjucks.configure(path.resolve('app', 'views'), {
   autoescape: true,
   express: app,
