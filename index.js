896311c2e609ee1026b2b80a2627a50d136773bc
Bruno Carpinelli <brunogcpinheiro@hotmail.com>
2018-04-23 13:10:49 -0300

    Initial configuration

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const nunjucks = require('nunjucks');
 
 const app = express();
 
+nunjucks.configure(path.resolve('app', 'views'), {
+  autoescape: true,
+  express: app,
+});
+
+app.set('view engine', 'njk');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
