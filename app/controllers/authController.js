edbaed296b731a75ed01f5316f3ed14371f55adf
brunogcpinheiro <brunogcpinheiro@hotmail.com>
2018-04-25 09:00:59 -0300

    Creating sessions and errors handling

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -10,19 +10,56 @@ module.exports = {
     return res.render('auth/signup');
   },
 
-  async register(req, res) {
-    const { email } = req.body;
+  async register(req, res, next) {
+    try {
+      const { email } = req.body;
 
-    if (await User.findOne({ where: { email } })) {
-      req.flash('error', 'E-mail já cadastrado');
-      return res.redirect('back');
+      if (await User.findOne({ where: { email } })) {
+        req.flash('error', 'E-mail já cadastrado');
+        return res.redirect('back');
+      }
+
+      const password = await bcrypt.hash(req.body.password, 5);
+
+      await User.create({ ...req.body, password });
+
+      req.flash('success', 'Usuário cadastrado com sucesso!');
+      return res.redirect('/');
+    } catch (err) {
+      return next(err);
     }
+  },
 
-    const password = await bcrypt.hash(req.body.password, 5);
+  async authenticate(req, res, next) {
+    try {
 
-    await User.create({ ...req.body, password });
+      const { email, password } = req.body;
 
-    req.flash('success', 'Usuário cadastrado com sucesso!');
-    return res.redirect('/');
+      const user = await User.findOne({ where: { email } });
+
+      if (!user) {
+        req.flash('error', 'Usuário inexistente');
+        return res.redirect('back');
+      }
+
+      if (!await bcrypt.compare(password, user.password)) {
+        req.flash('error', 'Senha incorreta');
+        return res.redirect('back');
+      }
+
+      req.session.user = user;
+
+      return req.session.save(() => {
+        res.redirect('app/dashboard');
+      });
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  signout(req, res) {
+    return req.session.destroy(() => {
+      res.redirect('/');
+    });
   },
 };
