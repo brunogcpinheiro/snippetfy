1e9211bb99559e340623cdf03eef793dcb02932c
brunogcpinheiro <brunogcpinheiro@hotmail.com>
2018-04-25 16:03:46 -0300

    Adding Edit and Delete Routes - Finished App

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,7 @@ const routes = express.Router();
 const authController = require('./controllers/authController');
 const dashboardController = require('./controllers/dashboardController');
 const categoryController = require('./controllers/categoryController');
+const snippetController = require('./controllers/snippetController');
 const authMiddleWare = require('./middlewares/auth');
 const guestMiddleware = require('./middlewares/guest');
 
@@ -34,6 +35,14 @@ routes.get('/app/dashboard', dashboardController.index);
  */
 routes.get('/app/categories/:id', categoryController.show);
 routes.post('/app/categories/create', categoryController.store);
+
+/**
+ * Snippets
+ */
+routes.get('/app/categories/:categoryId/snippets/:id', snippetController.show);
+routes.post('/app/categories/:categoryId/snippets/create', snippetController.store);
+routes.put('/app/categories/:categoryId/snippets/:id', snippetController.update);
+routes.delete('/app/categories/:categoryId/snippets/:id', snippetController.destroy);
 
 routes.use((req, res) => res.render('errors/404'));
 
